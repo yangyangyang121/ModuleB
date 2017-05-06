2a1c905bb15ad1b6f3833e6d70d427601c71b66f
daniel.yang <daniel.yang@jinjiang.com>
2017-05-06 22:25:52 +0800

    first commit

---
+++ ModuleB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleB
+//
+//  Created by yangdd on 2017/5/6.
+//  Copyright © 2017年 yangdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
