584cf55ac49357aeb76a7bf89fa6263bd5505e03
daniel.yang <daniel.yang@jinjiang.com>
2017-05-10 15:56:31 +0800

    m

--- ModuleB/ModuleBIndexViewController.h
+++ ModuleB/ModuleBIndexViewController.h
@@ -1,8 +1,8 @@
 //
-//  ViewController.h
+//  ModuleBIndexViewController.h
 //  ModuleB
 //
-//  Created by yangdd on 2017/5/6.
+//  Created by yangdd on 2017/5/10.
 //  Copyright © 2017年 yangdd. All rights reserved.
 //
 
@@ -10,6 +10,4 @@
 
 @interface ModuleBIndexViewController : UIViewController
 
-
 @end
-
--- ModuleB/ModuleBIndexViewController.m
+++ ModuleB/ModuleBIndexViewController.m
@@ -1,8 +1,8 @@
 //
-//  ViewController.m
+//  ModuleBIndexViewController.m
 //  ModuleB
 //
-//  Created by yangdd on 2017/5/6.
+//  Created by yangdd on 2017/5/10.
 //  Copyright © 2017年 yangdd. All rights reserved.
 //
 
@@ -16,14 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view from its nib.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
